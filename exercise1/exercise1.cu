21b87183a0c707dc2c5ad477af7cfb98f0170a2a
Patrick Stiller <s7520458@e06912.infpool.loc>
2016-10-11 12:30:38 +0200

    Error Handling

--- exercise1/exercise1.cu
+++ exercise1/exercise1.cu
@@ -3,6 +3,18 @@
 #include<cuda.h>
 #define N 1000000
 
+#define HANDLE_ERROR( err )(handleCudaError( err, __FILE__, __LINE__ ) )
+
+int handleCudaError(cudaError_t cut,const char* file, int line)
+{
+	if(cut != cudaSuccess)
+		{
+		printf("%s %s %d \n",cudaGetErrorString(cut),__FILE__,__LINE__);
+		return -1 ;
+		}
+	return 0;  
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -11,7 +23,8 @@ int main(int argc, char* argv[])
 	int *b_host=NULL;
 	int *a_device_ptr = NULL ;
 	int *b_device_ptr = NULL ;
-	int correct_transfer = 1 ;
+
+	 	
 	
 
 	a_host=(int*)malloc(sizeof(int)*N);
@@ -21,43 +34,46 @@ int main(int argc, char* argv[])
 	for(unsigned int i = 0; i < N; ++i)
 	{
 		a_host[i] = i ;
+		b_host[i] = 0;
 	}
-	a_host=(int*)malloc(sizeof(int)*N);
-	b_host=(int*)malloc(sizeof(int)*N);
 
-	cudaMalloc(&a_device_ptr, sizeof(int)*N); // malloc of a_device
-	cudaMalloc(&b_device_ptr, sizeof(int)*N); // malloc of a_device
 
+	HANDLE_ERROR(cudaMalloc(&a_device_ptr, sizeof(int)*N)); // malloc of a_device
+	HANDLE_ERROR(cudaMalloc(&b_device_ptr, sizeof(int)*N)); // malloc of b_device
+	
 	//Transfer a_host to a_device
-	cudaMemcpy(a_device_ptr, a_host, sizeof(int)*N, cudaMemcpyHostToDevice);
+	HANDLE_ERROR(cudaMemcpy(a_device_ptr, a_host, sizeof(int)*N, cudaMemcpyHostToDevice));
 
 	//Transfer a_device to b_device
 
-	cudaMemcpy(b_device_ptr, a_device_ptr, sizeof(int)*N, cudaMemcpyDeviceToDevice);
+	HANDLE_ERROR(cudaMemcpy(b_device_ptr, a_device_ptr, sizeof(int)*N, cudaMemcpyDeviceToDevice));
 
 	//Transfer b_device to b_host
 
-	cudaMemcpy(b_host, b_device_ptr, sizeof(int)*N, cudaMemcpyDeviceToHost);
+	HANDLE_ERROR(cudaMemcpy(b_host, b_device_ptr, sizeof(int)*N, cudaMemcpyDeviceToHost));
 	
 	//transfercheck
 
 	for(unsigned int i = 0; i < N; ++i)
 	{
-		correct_transfer = correct_transfer & (a_host[i] == b_host[i]); 	
+		//correct_transfer = correct_transfer & (a_host[i] == b_host[i]); 	
+	if(a_host[i] != b_host[i]) {
+		printf("Incorrect result at %d with %d and %d\n", i, a_host[i], b_host[i]);
+		return -1 ;
+		}
 	}
+	
+	printf("Correct transfer \n");
 
-	if(correct_transfer)
-	{
-		printf("Correct Transfer");
-	}
-
-
-	cudaFree(a_device_ptr);
-	cudaFree(b_device_ptr);
+	HANDLE_ERROR(cudaFree(a_device_ptr));
+	HANDLE_ERROR(cudaFree(b_device_ptr));
 	free(a_host);
 	free(b_host);
 	
 	a_host= NULL;
 	b_host= NULL; 
 
+	return 0 ; 
+
 }
+
