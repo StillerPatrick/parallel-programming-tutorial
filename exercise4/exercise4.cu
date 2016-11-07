9720baf0fbfc61420b1c59528db700da3dedbd08
Patrick Stiller <p.stiller@ebs-z.de>
2016-11-07 17:31:11 +0100

    Matrix

--- exercise4/exercise4.cu
+++ exercise4/exercise4.cu
@@ -8,16 +8,34 @@
 
 
 
+
+
 __global__ void diadic_Product (int n, int *a,int *b, int *erg)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x; // Calculate current Thread
-	int j = blockIdx.y * blockDim.y + threadIdx.y; 
+	int i = blockIdx.x * blockDim.x + threadIdx.x; // Calculate current Thread in x
+	int j = blockIdx.y * blockDim.y + threadIdx.y; // Calculate current Tread in y
 	
 	if(i < n && j < n)
 	{	
 		
 		erg[i*n+j] = a[i]*b[j]; 	
+	
 	}
+}
+
+__global__ void matrixProduct(int n, int *a, int *b, int *c)
+{
+		int i = blockIdx.x * blockDim.x + threadIdx.x; // Calculate current Thread in x
+		int j = blockIdx.y * blockDim.y + threadIdx.y; // Calculate current Tread in y
+		
+		int scalarProduct = 0; 
+		for(int k = 0; k < n; ++k)
+		{
+			scalarProduct += a[i*n+k]* b[k*n+j];
+		}
+		c[i*n+j] = scalarProduct ;
+	
+	
 }
 
 int handleCudaError(cudaError_t cut,const char* file, int line)
@@ -30,7 +48,27 @@ int handleCudaError(cudaError_t cut,const char* file, int line)
 	return 0;  
 }
 
-
+void matrixProduct(int n, int* a, int* b ,int *erg)
+{
+	for (int i = 0 ; i < n ; ++i)
+	{
+		
+		for(int j = 0 ; j < n; ++j)
+		{
+			int scalarProduct = 0; 
+			for(int k = 0 ; k < n ; ++k)
+			{
+				//sclarproduct of i'th row and j'th collumn 
+				int scalarProduct += a[i*n+k]* b[k*n+j] ;
+			}
+			
+			erg[i*n+j]= scalarProduct;
+		}
+		
+	}
+	
+	
+}
 
 
 
@@ -89,8 +127,8 @@ int main(int argc, char**args)
 	
 	int n = atoi(args[1]) ;
 	time_t t;
-	int *a = (int *) malloc( sizeof(int)*n);
-	int *b = (int *) malloc( sizeof(int)*n);
+	int *a = (int *) malloc( sizeof(int)*n*n);
+	int *b = (int *) malloc( sizeof(int)*n*n);
 	int *c = (int *) malloc( sizeof(int)*n*n);
 	int *c_t = (int *) malloc( sizeof(int)*n*n);
 
@@ -102,7 +140,7 @@ int main(int argc, char**args)
 	HANDLE_ERROR(cudaMalloc(&a_d, sizeof(int)*n)); // malloc of a_device
 	HANDLE_ERROR(cudaMalloc(&b_d, sizeof(int)*n)); // malloc of b_device
 	HANDLE_ERROR(cudaMalloc(&c_d, sizeof(int)*n*n)); // malloc of c_device 
-
+	double time1=0.0, tstart;      // time measurment variables
 	// random init 
 	time(&t);
 	srand((unsigned int)t);   
@@ -110,16 +148,16 @@ int main(int argc, char**args)
 
 	for(int i = 0; i < n; ++i)
 		{
-		
-
-					a[i] = rand() % 5 ;	
-					b[i]= rand() % 5 ; 		
-				
+			for (int j= 0; j < n ; ++j)
+			{
+					a[i*n+j] = rand() % 5 ;	
+					b[i*n+j]= rand() % 5 ; 		
+			}	
 		
 		}
 
 	//Transfer a_host to a_device
-	HANDLE_ERROR(cudaMemcpy(a_d, a, sizeof(int)*n, cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(a_d, a, sizeof(int)*n*n, cudaMemcpyHostToDevice));
 		
 	//Transfer b_host to b_device
 	HANDLE_ERROR(cudaMemcpy(b_d, b, sizeof(int)*n, cudaMemcpyHostToDevice));
@@ -138,14 +176,23 @@ int main(int argc, char**args)
 	
 	
 	printf("====== Result of matrix multiplication =====\n");
+	tstart = clock();              // start 
 	diadicProduct(n,a,b,c);
+	time1 = clock() - tstart;     // end
+	time1 = time1/CLOCKS_PER_SEC;  // rescale to seconds
 	
 
+	cudaEvent_t start, stop;
+	float time;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 	
+	cudaEventRecord(start, 0);
 	dim3 block(16,16,1);
 	dim3 grid(ceil((float)n/(float)16),ceil((float)n/(float)16),1);
 	diadic_Product<<<grid,block>>>(n,a_d,b_d,c_d);
-
+	HANDLE_ERROR(cudaEventRecord(stop, 0));
+	HANDLE_ERROR(cudaEventSynchronize(stop));
 	HANDLE_ERROR(cudaMemcpy(c_t, c_d, sizeof(int)*n*n, cudaMemcpyDeviceToHost));
 	printf("====== Result of matrix multiplication on Kernel=====\n");
 	
@@ -159,10 +206,12 @@ int main(int argc, char**args)
 				}
 		}
 
-	
-	
-	
-
+	HANDLE_ERROR(cudaFree(a_d));
+	HANDLE_ERROR(cudaFree(b_d));
+	HANDLE_ERROR(cudaFree(c_d));
+	cudaEventElapsedTime(&time, start, stop);
+	printf ("Time for the kernel-diadicProduct: %f msec\n", time );
+	printf ("Time for the CPU -diadicProduct %d msec \n",time1 *1000);
 
 	return 0;  	
 
